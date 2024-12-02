cf8bc24646b4d31fd57dc8b74189e6327916675d
alexanderfer30 <rugamasleenin@gmail.com>
2024-12-02 15:45:15 -0600

    Controladores

---
+++ LeeninRugamasMVC/Controllers/CategoriaController.cs
@@ -0,0 +1,46 @@
+﻿using LeeninRugamasMVC.BL;
+using LeeninRugamasMVC.EN;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeeninRugamasMVC.Controllers
+{
+    [Route("categorias")]
+    public class CategoriaController : Controller
+    {
+        private readonly CategoriaBL _categoriaBL;
+
+        public CategoriaController(CategoriaBL categoriaBL)
+        {
+            _categoriaBL = categoriaBL;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var categorias = _categoriaBL.Listar();
+            return View(categorias);
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Categoria categoria)
+        {
+            _categoriaBL.Insertar(categoria);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Categoria categoria)
+        {
+            _categoriaBL.Actualizar(categoria);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            _categoriaBL.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ LeeninRugamasMVC/Controllers/ProductoController.cs
@@ -0,0 +1,46 @@
+﻿using LeeninRugamasMVC.BL;
+using LeeninRugamasMVC.EN;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeeninRugamasMVC.Controllers
+{
+    [Route("productos")]
+    public class ProductoController : Controller
+    {
+        private readonly ProductoBL _productoBL;
+
+        public ProductoController(ProductoBL productoBL)
+        {
+            _productoBL = productoBL;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var productos = _productoBL.Listar();
+            return View(productos);
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Producto producto)
+        {
+            _productoBL.Insertar(producto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Producto producto)
+        {
+            _productoBL.Actualizar(producto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            _productoBL.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
